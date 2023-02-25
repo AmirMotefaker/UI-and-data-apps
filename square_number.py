baee717004836bd59ff3d344c7499adae93e7c01
Amir Motefaker <amotef@gmail.com>
2023-02-25 18:37:47 +0330

    def add_logo()

--- square_number.py
+++ square_number.py
@@ -35,6 +35,34 @@ st.write(x, "multiply 8 is",  x * 8)
 st.write(x, "multiply 9 is",  x * 9)
 
 
+
+
+# -----------------------Logo-------------------
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(http://placekitten.com/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
+
 # --------------------------------------
 # third step:
 # and Now run it to open the app in browser with this code:
