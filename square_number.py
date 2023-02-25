d1a3d8256228d755e11238ee54558c832d7c96b5
Amir Motefaker <amotef@gmail.com>
2023-02-25 17:09:51 +0330

    Added square_number.py

---
+++ square_number.py
@@ -0,0 +1,23 @@
+# Calculate the square of any number
+
+# ---------------------------------------
+# first step:
+# Open a terminal and run:
+
+# $ pip install streamlit
+# $ streamlit hello
+
+# If this opens our sweet Streamlit Hello app in your browser, you're all set! 
+
+# ---------------------------------------
+# second step:
+# Create a new file square_number.py with the following code:
+
+import streamlit as st
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
+
+# --------------------------------------
+# third step:
+# and Now run it to open the app in browser with this code:
+# $ streamlit run square_number.py
